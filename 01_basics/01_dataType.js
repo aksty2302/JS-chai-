7281e3f1f5c6887d027b1e5870fca7adb14e7a45
aksty2302 <akshaytiwari26100@gmail.com>
2024-07-16 03:50:59 +0000

    datatype

---
+++ 01_basics/01_dataType.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all js code as newer version 
+
+// alert(3 + 3) // we are using node, not browser
+
+// numbers => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined 
+// special => unique
+
+
+
+// object
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
